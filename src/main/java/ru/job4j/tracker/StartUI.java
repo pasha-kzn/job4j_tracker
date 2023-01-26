82f2e5ae83bf8220a23d95d9299b9035b18c8567
pasha-kzn <pavelmatweeff@yandex.ru>
2023-01-26 20:51:47 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 import java.util.zip.DataFormatException;
 
-public class StartUI{
+public class StartUI {
     public static void main(String[] args) throws InterruptedException {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
