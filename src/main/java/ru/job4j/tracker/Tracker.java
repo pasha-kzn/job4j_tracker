33796b9beefcdc55eafafa2eecf8ef1539ebcff8
pasha-kzn <pavelmatweeff@yandex.ru>
2023-02-01 20:41:02 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +22,36 @@ public class Tracker {
                 break;
             }
         }
+        return rsl;
+    }
+    /*Метод public Item[] findAll() возвращает копию массива items без null элементов (без пустых ячеек).*/
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            String name = items[i].getName();
+            if (name != null) {
+                rsl[i] = new Item(name);
+                size++;
+            }
+            break;
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        return rsl;
+    }
+    /*Метод public Item[] findByName(String key) проверяет в цикле все элементы массива items, сравнивая name (используя метод getName класса Item)
+     с аргументом метода String key. Элементы, у которых совпадает name, копирует в результирующий массив и возвращает его.
+     Алгоритм этого метода аналогичен методу findAll.*/
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && key.equals(items[i].getName())) {
+                rsl[i] = new Item(key);
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
         return rsl;
     }
 }
