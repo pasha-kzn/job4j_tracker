7a4abd527f978fafc8e2b686f74408a110ee6e57
pasha-kzn <pavelmatweeff@yandex.ru>
2023-02-08 08:56:41 +0300

    2.0. Чтение из консоли. Класс Scanner. [#179514 #389463]

---
+++ src/main/java/ru/job4j/tracker/io/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.io;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Добро пожаловать. Как Вас зовут? \n");
+        String name = input.nextLine();
+        System.out.println(name + ", рад Вас видеть!");
+    }
+}
