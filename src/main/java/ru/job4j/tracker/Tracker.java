88dcf8a322c6d00dd988349a11ade149e00867fa
pasha-kzn <pavelmatweeff@yandex.ru>
2023-02-09 21:53:39 +0300

    Чекстайл

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -17,7 +17,7 @@ public class Tracker {
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index  != -1 ? items[index] : null;
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
