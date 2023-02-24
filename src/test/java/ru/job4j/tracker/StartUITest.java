ab59f0d94bb07335e218471117c82680f7ce5a9f
pasha-kzn <pavelmatweeff@yandex.ru>
2023-02-25 00:06:14 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,42 +4,4 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"BlaBlaBla", "TraTaTa"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        StartUI.createItem(input, tracker);
-        Item created1 = tracker.findAll()[0];
-        Item created2 = tracker.findAll()[1];
-        Item expected1 = new Item("BlaBlaBla");
-        Item expected2 = new Item("TraTaTa");
-        assertThat(expected1.getName()).isEqualTo(created1.getName());
-        assertThat(expected2.getName()).isEqualTo(created2.getName());
-    }
-
-    @Test
-    public void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        assertThat(tracker.findById(item.getId())).isNull();
-    }
 }
