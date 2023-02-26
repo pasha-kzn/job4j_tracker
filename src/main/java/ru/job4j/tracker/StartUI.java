ce586112f4fa92fe9ac19588175df17f6d846497
pasha-kzn <pavelmatweeff@yandex.ru>
2023-02-26 22:06:12 +0300

    12.2. Тесты вывода на консоль [#33585]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -18,7 +18,7 @@ public class StartUI {
     }
 
     private void showMenu(UserAction[] actions) {
-        out.println("Menu.");
+        out.println("Menu:");
         for (int index = 0; index < actions.length; index++) {
            out.println(index + ". " + actions[index].name());
         }
@@ -31,7 +31,7 @@ public class StartUI {
         UserAction[] actions = {
                 new CreateAction(output), new ShowAllAction(output), new ReplaceAction(output),
                 new DeleteAction(output), new FindByIdAction(output),
-                new FindByNameAction(output), new Exit()
+                new FindByNameAction(output), new ExitAction(output)
         };
         new StartUI(output).init(input, tracker, actions);
     }
