8a67b0fb31ce78eceb169779c51d99da76b28257
pasha-kzn <pavelmatweeff@yandex.ru>
2023-04-26 19:59:41 +0300

    0. Map, HashMap [#212652 #412300]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("pavelmatweeff@yandex.ru", "Мatveev Pavel");
+        map.put("ilyamatweev@yandex.ru", "Мatveev Ilya P.");
+        map.put("polinamatweeva@yandex.ru", "Мatveeva Polina P.");
         map.put("pavelmatweeff@yandex.ru", "Мatveev Pavel A.");
         for (String key : map.keySet()) {
             System.out.println(map.get(key));
