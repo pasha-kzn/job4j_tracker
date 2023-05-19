c5f785e31702a07a8677e4831abfd8afa796ec59
pasha-kzn <pavelmatweeff@yandex.ru>
2023-05-19 17:26:42 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,6 +2,10 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель банковского счета
+ * Модель содержит поля: баланс и реквизиты
+ */
 public class Account {
     private String requisite;
     private double balance;
--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -8,17 +8,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу простейшего сервиса банковского приложения
+ * @author Pavel Matveev
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Хранение информации о клиенте и его счетах хранится в коллекции типа HashMap
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод принимает на вход пользователя и добавляет его в коллекцию пользователей, при
+     * условии, что такого пользователя в коллекции еще нет
+     * @param user пользователь, добавляемый в коллекцию
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Метод принимает на вход паспортные данные, определяет пользователя, и удаляет его из коллекции пользователей
+     * @param passport паспортные данные
+     * @return возвращает true при успешном удалении пользователя из коллекции пользователей
+     */
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
     }
 
+    /**
+     * Метод принимает на вход паспотрные данные с данными счета, и создает новый счет клиенту
+     * @param passport паспортные данные
+     * @param account данные счета, включающие реквизиты и баланс счета
+     */
     public void addAccount(String passport, Account account) {
         var user = findByPassport(passport);
         if (user != null && !getAccounts(user).contains(account)) {
@@ -26,6 +49,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод принимает на вход паспортные данные и находит пользователя в системе, если он существует
+     * @param passport паспортные данные
+     * @return u/null пользователь, если он найден
+     */
     public User findByPassport(String passport) {
         for (User u : users.keySet()) {
             if (passport.equals(u.getPassport())) {
@@ -35,6 +63,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод принимает на вход паспортные данные и реквизиты и находит счет, если он существует
+     * @param passport паспортные данные
+     * @param requisite реквизиты
+     * @return account/null счет если он найден
+     */
     public Account findByRequisite(String passport, String requisite) {
         var user = findByPassport(passport);
         if (user != null) {
@@ -48,6 +82,16 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод принимает на вход паспортные данные и реквизиты счета отправителя/получателя, сумму перевода. При
+     * корректных входящих данных перевод средств происходит с изменением сумм на счетах отправителя/получателя
+     * @param srcPassport паспортные данные отправителя перевода
+     * @param srcRequisite реквизиты счета списания
+     * @param destPassport паспортные данные получателя перевода
+     * @param destRequisite реквизиты счета пополнения
+     * @param amount сумма перевода
+     * @return rsl результат выполнения операции
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         var srcAccount = findByRequisite(srcPassport, srcRequisite);
@@ -62,6 +106,11 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод принимает на вход пользователя и находит все счета пользователя
+     * @param user пользователь
+     * @return List<Account> список счетов пользователя
+     */
     public List<Account> getAccounts(User user) {
         return users.get(user);
     }
