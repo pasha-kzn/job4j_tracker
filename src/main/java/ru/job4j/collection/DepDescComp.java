39240a0470d53dc260f9241779ae01d219f72ba2
pasha-kzn <pavelmatweeff@yandex.ru>
2024-01-04 12:27:27 +0300

    3. Отсортировать департаменты [#34352 #430586]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,15 +7,17 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftArray = left.split("/");
         String[] rightArray = right.split("/");
-        int length = Math.min(leftArray.length, rightArray.length);
-        for (int i = 0; i < length; i++) {
-            if (leftArray[0].compareTo(rightArray[0]) != 0) {
-                return right.compareTo(left);
-            }
-            if (leftArray[i].compareTo(rightArray[i]) != 0) {
-                return left.compareTo(right);
-            }
-        }
-        return left.compareTo(right);
+//        int length = Math.min(leftArray.length, rightArray.length);
+//        for (int i = 0; i < length; i++) {
+//            if (leftArray[0].compareTo(rightArray[0]) != 0) {
+//                return right.compareTo(left);
+//            }
+//            if (leftArray[i].compareTo(rightArray[i]) != 0) {
+//                return left.compareTo(right);
+//            }
+//        }
+//        return left.compareTo(right);
+        int result = rightArray[0].compareTo(leftArray[0]);
+        return result != 0 ? result : left.compareTo(right);
     }
 }
