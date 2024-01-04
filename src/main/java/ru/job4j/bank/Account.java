e8d595b43db5baecfc875bb14ae860157e23481f
pasha-kzn <pavelmatweeff@yandex.ru>
2024-01-04 12:36:10 +0300

    3. Генерация документации по JavaDoc [#313880 #418550]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -32,14 +32,14 @@ public class Account {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
-        if (obj == null || getClass() != obj.getClass()) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Account account = (Account) obj;
+        Account account = (Account) o;
         return Objects.equals(requisite, account.requisite);
     }
 
