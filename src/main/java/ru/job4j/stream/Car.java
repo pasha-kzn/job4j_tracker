c6adda6cc8266797ec302518264c71d3dd96bf2e
Pavel Matveev KZN <pavelmatweeff@yandex.ru>
2024-11-28 10:00:54 +0300

    18. Шаблоны проектирования в Stream API [#310012 #523755]

--- src/main/java/ru/job4j/stream/Car.java
+++ src/main/java/ru/job4j/stream/Car.java
@@ -15,15 +15,14 @@ public class Car {
 
     @Override
     public String toString() {
-        return "Car{" +
-                "brand='" + brand + '\'' +
-                ", model='" + model + '\'' +
-                ", created=" + created +
-                ", volume=" + volume +
-                ", color='" + color + '\'' +
-                '}';
+        return "Car{"
+                + "brand='" + brand + '\''
+                + ", model='" + model + '\''
+                + ", created=" + created
+                + ", volume=" + volume
+                + ", color='" + color + '\''
+                + '}';
     }
-
 
     static class Builder {
         private String brand;
