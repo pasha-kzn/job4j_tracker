dda2b18b6cedc1c4dcc8b43dfde5ef84527e9586
Pavel Matveev KZN <pmatveev@dpd.ru>
2025-04-05 10:36:25 +0300

    GPT. Нейросети спешат на помощь. [#505156]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short heightMan) {
+        double result = (heightMan - 100) * 1.15;
+        return result;
+    }
+
+    public static double womanWeight(short heightWoman) {
+        double result = (heightWoman - 110) * 1.15;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short heightMan = 187;
+        short heightWoman = 170;
+        double man = Fit.manWeight(heightMan);
+        System.out.println("Man 187 is " + man);
+        double woman = Fit.womanWeight(heightWoman);
+        System.out.println("Woman 170 is " + woman);
+    }
+}
