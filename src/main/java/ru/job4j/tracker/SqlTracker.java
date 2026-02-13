ab2cf1803b30602a3f9e3c595f2c6571132051f2
Pavel Matveev KZN <pmatveev@dpd.ru>
2026-02-13 07:35:14 +0300

    1. Liquibase. Установка. [#504892]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -41,7 +41,7 @@ public class SqlTracker implements Store {
     }
 
     private void init() {
-        try (InputStream in = SqlTracker.class.getClassLoader()
+        try (InputStream input = SqlTracker.class.getClassLoader()
                 .getResourceAsStream("db/liquibase.properties")) {
             Properties config = new Properties();
             config.load(input);
